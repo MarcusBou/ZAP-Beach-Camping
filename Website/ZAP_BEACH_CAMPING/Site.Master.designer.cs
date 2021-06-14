6d125e6ace768823435e914dfbd97ec3ac14c8a0
DanielVuust <Daniel.vuust@gmail.com>
2021-06-14 21:38:26 +0200

    .

--- Website/ZAP_BEACH_CAMPING/Site.Master.designer.cs
+++ Website/ZAP_BEACH_CAMPING/Site.Master.designer.cs
@@ -7,19 +7,17 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ZAP_BEACH_CAMPING
-{
-
-
-    public partial class SiteMaster
-    {
-
+namespace ZAP_BEACH_CAMPING {
+    
+    
+    public partial class SiteMaster {
+        
         /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
+        /// To modify, move the field declaration from the designer file to a code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
