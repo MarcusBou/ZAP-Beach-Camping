477eee0f8f40f677e833b54126110d64d2e04488
DanielVuust <Daniel.Vuust@gmail.com>
2021-06-17 09:47:57 +0200

    .

--- ZAP_BeachCamping/ZAP_BeachCamping/Booking.cs
+++ ZAP_BeachCamping/ZAP_BeachCamping/Booking.cs
@@ -8,6 +8,35 @@ namespace ZAP_BeachCamping
 {
     class Booking
     {
-        private int
+        private DateTime startDate;
+        private DateTime endDate;
+        private int adult;
+        private int child;
+        private int dog;
+        private int adgangBadelandBarn;
+        private int adgangBadelandVoksen;
+        private int cykelleje;
+        private int morgenkompletBarn;
+        private int morgenkompletVoksen;
+        private int sengelinned;
+        private int slutrengøring;
+        private int hasView;
+
+        public Booking(DateTime startDate, DateTime endDate, int adult, int child, int dog, int adgangBadelandBarn, int adgangBadelandVoksen, int cykelleje, int morgenkompletBarn, int morgenkompletVoksen, int sengelinned, int slutrengøring, int hasView)
+        {
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.adult = adult;
+            this.child = child;
+            this.dog = dog;
+            this.adgangBadelandBarn = adgangBadelandBarn;
+            this.adgangBadelandVoksen = adgangBadelandVoksen;
+            this.cykelleje = cykelleje;
+            this.morgenkompletBarn = morgenkompletBarn;
+            this.morgenkompletVoksen = morgenkompletVoksen;
+            this.sengelinned = sengelinned;
+            this.slutrengøring = slutrengøring;
+            this.hasView = hasView;
+        }
     }
 }
